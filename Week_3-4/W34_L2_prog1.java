36e868915998a4e068d6374fab2f4c58ca50ee74
jitheshgokul <jitheshg111@gmail.com>
2026-03-11 00:05:40 +0530

    Added First program for Week_3-4

---
+++ Week_3-4/W34_L2_prog1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class W34_L2_prog1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (number > 0) {
+            for (int i = 1; i <= number; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i + " is an even number");
+                } else {
+                    System.out.println(i + " is an odd number");
+                }
+            }
+        } else {
+            System.out.println("Please enter a natural number (greater than 0).");
+        }
+
+        input.close();
+    }
+}
