cef92b8ee89354fb05a4a611549a074212f453f9
jitheshgokul <jitheshg111@gmail.com>
2026-03-10 22:07:03 +0530

    Added Second program for Week_1-2

---
+++ Week_1-2/L1_prog2.java
@@ -0,0 +1,9 @@
+public class L1_prog2 {
+	public static void main(String[] args){
+		int maths = 94;
+		int physics = 95;
+		int chemistry = 96;
+		int percentage = (maths+physics+chemistry)/3;
+		System.out.println("Sam’s average mark in PCM is "+percentage+"%");
+	}
+}
