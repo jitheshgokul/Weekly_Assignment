e1a7fd3500820bb1c714a037994d9570ff1ddabf
jitheshgokul <jitheshg111@gmail.com>
2026-03-10 22:10:25 +0530

    Added Third program for Week_1-2

---
+++ Week_1-2/L1_prog3.java
@@ -0,0 +1,9 @@
+public class L1_prog3 {
+    public static void main(String[] args) {
+        double kilometers = 10.8;
+        double times = 1.6;
+        double miles = kilometers * times;
+
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
